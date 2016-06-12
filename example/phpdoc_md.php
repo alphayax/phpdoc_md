91f8ad2eba8e52074a03f167ae195906dcd78fd9
alphayax <alphayax@gmail.com>
2016-06-12 16:06:08 +0200

    Update doc & exemples

--- example/phpdoc_md.php
+++ example/phpdoc_md.php
@@ -2,8 +2,8 @@
 
 require_once '../vendor/autoload.php';
 
-$srcDir     = __DIR__.'/../srcss';
+$srcDir     = __DIR__.'/../src';
 $namespace  = 'alphayax\mdGen';
 
 $gen = new \alphayax\mdGen\MdGen( $srcDir, $namespace);
-$gen->generate( '../docs/md_gen');
+$gen->generate( '../docs');
